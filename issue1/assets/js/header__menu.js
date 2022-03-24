110ab674a59882d6d34cf1a9293c8bf4ad5d8595
Ivan Akulov <nraog@yandex.ru>
2022-03-24 15:28:46 +0300

    Исправление нейминга

--- issue1/assets/js/header__menu.js
+++ issue1/assets/js/header__menu.js
@@ -1,13 +1,13 @@
-const menu_item = document.querySelectorAll(".header__menu .menu-item")
-let active_menu_item = menu_item[0]
+const menuItems = document.querySelectorAll(".header__menu .menu-item")
+let activeMenuItem = menuItems[0]
 
-menu_item.forEach(el => {
+menuItems.forEach(el => {
     el.onclick = e => {
-        active_menu_item.classList.remove("active")
-        active_menu_item = e.target
-        active_menu_item.classList.add("active")
+        activeMenuItem.classList.remove("active")
+        activeMenuItem = e.target
+        activeMenuItem.classList.add("active")
     }
 })
 
 // set first menu-item as default
-active_menu_item.classList.add("active")
+activeMenuItem.classList.add("active")

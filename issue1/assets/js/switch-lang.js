83bb22e117b6c38ea930c297e1660b094617e6f4
Ivan Akulov <nraog@yandex.ru>
2022-03-24 15:43:15 +0300

    Изменение переключателя языка

--- issue1/assets/js/switch-lang.js
+++ issue1/assets/js/switch-lang.js
@@ -1,60 +1,21 @@
-const prev_lang_ru = document.getElementById("prev__switch_ru");
-const prev_lang_eng = document.getElementById("prev__switch_eng");
+const arraySwitcherLangRu = document.querySelectorAll(".switcher-lang[data-lang='ru']")
+const arraySwitcherLangEng = document.querySelectorAll(".switcher-lang[data-lang='eng']")
 
-const menu_lang_ru = document.getElementById("menu__switch_ru");
-const menu_lang_eng = document.getElementById("menu__switch_eng");
-
-const prev_langs = [
-    prev_lang_eng,
-    prev_lang_ru
-];
-const menu_langs = [
-    menu_lang_eng,
-    menu_lang_ru
-];
-const all_langs = [
-    prev_langs,
-    menu_langs
-];
-
-let current_lang = "";
-
-function remove_class_to_active_lang(class_name) {
-    all_langs.forEach(arr_langs => {
-        arr_langs.forEach(el => el.classList.remove(class_name))
-    })
+const setLang = (arrayActiveLang, arrayInactiveLang) => {
+  arrayInactiveLang.forEach(el => el.classList.remove("active"))
+  arrayActiveLang.forEach(el => el.classList.add("active"))
 }
 
-function add_class_to_active_lang(class_name) {
-    all_langs.forEach(arr_langs => {
-        arr_langs.forEach(el => {
-            if (el.dataset.lang == current_lang) el.classList.add(class_name)
-        })
-    })
+const setClickEvent = (arrayEl, eventHandler) => {
+  arrayEl.forEach(el => el.addEventListener("click", eventHandler))
 }
 
-function set_lang(lang) {
-    remove_class_to_active_lang("active")
-    current_lang = lang
-    add_class_to_active_lang("active", current_lang)
-}
+setClickEvent(arraySwitcherLangEng, () => {
+  setLang(arraySwitcherLangEng, arraySwitcherLangRu)
+})
+setClickEvent(arraySwitcherLangRu, () => {
+  setLang(arraySwitcherLangRu, arraySwitcherLangEng)
+})
 
-function lang_click(e) {
-    const el = e.target
-    const el_lang = el.dataset.lang
-    if (current_lang !== el_lang) set_lang(el_lang)
-}
-
-function main() {
-    all_langs.forEach(arr_langs => {
-        arr_langs.forEach(lang => {
-            lang.onclick = lang_click
-        })
-    })
-
-//  set eng lang as default
-    set_lang("eng")
-}
-
-main()
-
+// set eng as default
+setLang(arraySwitcherLangEng, [])

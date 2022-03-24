f6e525bfd6d79a4638aa1ae87a46f78eedd564ef
Ivan Akulov <nraog@yandex.ru>
2022-03-24 15:41:06 +0300

    Исправление ошибки

--- issue1/assets/js/header__menu.js
+++ issue1/assets/js/header__menu.js
@@ -1,12 +1,12 @@
-const menuItems = document.querySelectorAll(".header__menu .menu-item")
-let activeMenuItem = menuItems[0]
+const headerMenuItems = document.querySelectorAll(".header__menu .menu-item")
+let activeMenuItem = headerMenuItems[0]
 
-menuItems.forEach(el => {
-    el.onclick = e => {
-        activeMenuItem.classList.remove("active")
-        activeMenuItem = e.target
-        activeMenuItem.classList.add("active")
-    }
+headerMenuItems.forEach(el => {
+  el.addEventListener("click", e => {
+    activeMenuItem.classList.remove("active")
+    activeMenuItem = e.target
+    activeMenuItem.classList.add("active")
+  })
 })
 
 // set first menu-item as default
